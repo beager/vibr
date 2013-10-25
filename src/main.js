e343568c40fef1df0c5cab0d383506722d902900
Bill Eager <bill@tumblr-MacBookPro-d0e47d.(none)>
2013-10-25 02:13:09 -0400

    nice lookin things

--- src/main.js
+++ src/main.js
@@ -11,22 +11,6 @@ $(document).ready(function(){
 			"glockenspiel"
 		],
 		callback: function() {
-			function playNote(name, data) {
-				var channel = midi_channel(name);
-				var scale = current_channel.midi[channel].scale || 'major';
-				var pitch = get_note_from_scale(data.note, scale);
-				pitch += current_channel.transpose;
-				MIDI.noteOn(0, pitch, data.velocity, 0);
-				MIDI.noteOff(0, pitch, data.duration);
-			}
-
-			function playOpenTSDB(data) {
-				var freq_total = 0;
-				for (var i in data) {
-					freq_total += data[i][1];
-				}
-				changeBassNote(Math.floor(freq_total) * 4);
-			}
 
 			window.addEventListener("message", function(event) {
 				switch(event.data.type) {
@@ -52,4 +36,21 @@ var bassNotes = ['C2', 'D2', 'E2', 'G2', 'C3'];
 function changeBassNote(note) {
 	note = note || bassNotes[Math.floor(Math.random()*bassNotes.length)];
 	synth.noteSlide(note, 0.02);
+}
+
+function playNote(name, data) {
+	var channel = midi_channel(name);
+	var scale = current_channel.midi[channel].scale || 'major';
+	var pitch = get_note_from_scale(data.note, scale);
+	pitch += current_channel.transpose;
+	MIDI.noteOn(0, pitch, data.velocity, 0);
+	MIDI.noteOff(0, pitch, data.duration);
+}
+
+function playOpenTSDB(data) {
+	var freq_total = 0;
+	for (var i in data) {
+		freq_total += data[i][1];
+	}
+	changeBassNote(Math.floor(freq_total) * 4);
 }
