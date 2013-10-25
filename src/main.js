d2bb18369cda6e384be6320816e88de75fa17d76
Bill Eager <bill@tumblr-MacBookPro-d0e47d.(none)>
2013-10-24 20:52:05 -0400

    src js changes idfk

--- src/main.js
+++ src/main.js
@@ -1,32 +1,15 @@
+var synth;
+
 $(document).ready(function(){
 	MIDI.loadPlugin({
 		soundfontUrl: "./soundfont/",
 		instrument: "acoustic_grand_piano",
 		callback: function() {
 
-			// play the note
-			MIDI.setVolume(0, 127);
-			$('#button').click(function(e) {
-							var delay = 0; // play one note every quarter second
-					var note = 50; // the MIDI note
-					var velocity = 127; // how hard the note hits
-
-				console.log('hey');
-				MIDI.noteOn(0, note, velocity, delay);
-				MIDI.noteOn(0, note+4, velocity, delay);
-				MIDI.noteOn(0, note+7, velocity, delay);
-				MIDI.noteOn(0, note+12, velocity, delay);
-				MIDI.noteOff(0, note, delay + 5);
-				MIDI.noteOff(0, note+4, delay + 5);
-				MIDI.noteOff(0, note+7, delay + 5);
-				MIDI.noteOff(0, note+12, delay + 5);
-			});
-
 			MIDI.setReverbImpulseResponse('./ir/spatialized7.wav');
 
 			function playNote(data) {
 				var pitch = get_note_from_scale(data.note, 'pentatonic');
-				console.log(pitch);
 				MIDI.noteOn(0, pitch, data.velocity, 0);
 				MIDI.noteOff(0, pitch, data.duration);
 			}
@@ -37,4 +20,21 @@ $(document).ready(function(){
 		}
 	});
 
+  var audioContext = new webkitAudioContext();
+  audioContext.sampleRate = 44100;
+
+  synth = new MonoSynth(audioContext);
+
+  synth.noteOn('C2', 0.05);
+
+  setTimeout(changeBassNote, 12000);
+
 });
+
+var bassNotes = ['C2', 'D2', 'E2', 'G2', 'C3'];
+
+function changeBassNote() {
+	var note = bassNotes[Math.floor(Math.random()*bassNotes.length)];
+	synth.noteSlide(note, 0.05);
+	setTimeout(changeBassNote, 12000);
+}
